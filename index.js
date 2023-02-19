c347d493aef9a23a977c72ec85f002c02191705c
Chris Wang <chriswang8227@gmail.com>
2023-02-19 23:38:22 +0800

    hamburger button and navList

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ function mainFunc() {
     const element = document.getElementById("menu")
     
 }
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menuList");
+
+hamburger.addEventListener("click", () => {
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((item) =>
+	item.addEventListener("click", () => {
+		hamburger.classList.remove("active");
+		navMenu.classList.remove("active");
+	})
+);
